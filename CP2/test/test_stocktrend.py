cd77973865a6c23e55822d29a7deeb036910a43f
Hernan Y.Ke <yuriqiao@gmail.com>
2016-06-19 11:10:41 -0400

    Refactor methods

--- CP2/test/test_stocktrend.py
+++ CP2/test/test_stocktrend.py
@@ -6,12 +6,17 @@ from ..stocktrend import K
 class KTrendTest(unittest.TestCase):
     def setUp(self):
         self.ke = K("k")
-    def test_increase(self):
+    def given_a_series(self,prices):
         tss = [datetime(2016, 6, 18),datetime(2016, 6, 19),datetime(2016, 6, 20)]
-        prices = [1,2,3]
         for ts,price in zip(tss, prices):
             self.ke.update(ts,price)
+    def test_increasing_trend_is_true(self):
+        self.given_a_series([1,2,3])
         self.assertTrue(self.ke.is_increasing())
+    def test_decreasing_trend_is_true(self):
+        self.given_a_series([2,1,0])
+        self.assertFalse(self.ke.is_increasing())
+
 
 
 
