baf45c422d3bfef613ebe0e6434dcc28c58f7659
Hernan Y.Ke <yuriqiao@gmail.com>
2016-06-19 15:18:48 -0400

    using mock framework, note when function with parameters, using callee.assert_called_with

--- CP4/test/test_event.py
+++ CP4/test/test_event.py
@@ -1,25 +1,26 @@
 __author__ = 'Hernan Y.Ke'
 import unittest
+from unittest import mock
 from ..event import Event
 
-class Mock:
-    def __init__(self):
-        self.called = False
-        self.params = ()
-    def __call__(self, *args, **kwargs):
-        self.called=True
-        self.params=(args,kwargs)
+# class Mock:
+#     def __init__(self):
+#         self.called = False
+#         self.params = ()
+#     def __call__(self, *args, **kwargs):
+#         self.called=True
+#         self.params=(args,kwargs)
 class EventTest(unittest.TestCase):
     def test_listener_when_is_raised(self):
-        listener = Mock()
+        listener = mock.Mock()
         event = Event()
         event.connect(listener)
         event.fire()
         self.assertTrue(listener.called)
 
     def test_listener_params(self):
-        listener = Mock()
+        listener = mock.Mock()
         event = Event()
         event.connect(listener)
         event.fire(1,b='b')
-        self.assertEqual(((1,),{'b':'b'}),listener.params)
+        listener.assert_called_with(1,b='b')
