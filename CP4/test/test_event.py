69307d84c9cc231ffaf02522665bb7646b9f33cf
Hernan Y.Ke <yuriqiao@gmail.com>
2016-06-19 15:14:17 -0400

    create Mock

--- CP4/test/test_event.py
+++ CP4/test/test_event.py
@@ -1,23 +1,25 @@
 __author__ = 'Hernan Y.Ke'
 import unittest
 from ..event import Event
+
+class Mock:
+    def __init__(self):
+        self.called = False
+        self.params = ()
+    def __call__(self, *args, **kwargs):
+        self.called=True
+        self.params=(args,kwargs)
 class EventTest(unittest.TestCase):
     def test_listener_when_is_raised(self):
-        called = False
-        def listener():
-            nonlocal called
-            called =True
+        listener = Mock()
         event = Event()
         event.connect(listener)
         event.fire()
-        self.assertTrue(called)
+        self.assertTrue(listener.called)
 
     def test_listener_params(self):
-        params=()
-        def listener(*args,**kwargs):
-            nonlocal params
-            params=(args,kwargs)
+        listener = Mock()
         event = Event()
         event.connect(listener)
         event.fire(1,b='b')
-        self.assertEqual(((1,),{'b':'b'}),params)
+        self.assertEqual(((1,),{'b':'b'}),listener.params)
